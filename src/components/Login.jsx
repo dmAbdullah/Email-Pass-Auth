639bd28ad2d6bce12539f61dbfe312606dd530ec
dmAbdullah <dmabdullah8@gmail.com>
2023-04-17 01:00:08 +0600

    validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,29 @@ const Login = () => {
     return (
         <div>
             <h2>this is login page</h2>
+            <form>
+      <div className="form-group">
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          placeholder="Enter username"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          placeholder="Password"
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Login
+      </button>
+    </form>
         </div>
     );
 };
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,36 +1,56 @@
 import React, { useState } from "react";
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import app from './Firebase.config'
-
-const auth = getAuth(app);
+import app from "./Firebase.config";
 
 const Register = () => {
-  const [email, setEmail] = useState("");
+  const auth = getAuth(app);
+  //   const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    const email= event.target.email.value
-    const pass= event.target.password.value
-    console.log(email, pass);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+    // validate
+    if (!/(?=.*[A-Z])/.test(password)) {
+      setError("Please add at least one uppercase");
+      return;
+    }
+     
+    else if (!/(?=.*[0-9].*[0-9])/.test(password)) {
+      setError("Please add at least two numbers");
+      return;
+    } 
+    
+    else if (password.length < 6) {
+      setError("Please add at least 6 characters in your password");
+      return;
+    }
     // create user in firebase
-    createUserWithEmailAndPassword(auth, email, pass)
-    .then(result => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
         const loggedUser = result.user;
         console.log(loggedUser);
-    })
-    .catch(error => {
-        console.error(error);
-    })
-  }
+        setError("");
+        event.target.reset();
+        // setSuccess('User has been successfully created')
+      })
+      .catch((error) => {
+        console.error(error.message);
+        setError(error.message);
+        setSuccess("");
+      });
+  };
 
   const handleEmailChange = (event) => {
     // console.log(event.target.value);
-    setEmail(event.target.value);
+    // setEmail(event.target.value);
   };
 
   const handlePasswordBlur = (event) => {
     // console.log(event.target.value);
-
   };
   return (
     <div>
@@ -42,6 +62,7 @@ const Register = () => {
           name="email"
           id="email"
           placeholder="Your Email"
+          required
         />
         <br />
         <input
@@ -49,11 +70,14 @@ const Register = () => {
           type="password"
           name="password"
           id="password"
-          placeholder="Your Password "
+          placeholder="Your Password"
+          required
         />
         <br />
         <input type="submit" value="Register" />
       </form>
+      <p className="text-danger">{error}</p>
+      <p className="text-success">{success}</p>
     </div>
   );
 };
