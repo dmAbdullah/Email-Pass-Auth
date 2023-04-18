8148f7f1380d99cff056340ea4ba3f18c4c210ae
dmAbdullah <dmabdullah8@gmail.com>
2023-04-18 11:26:13 +0600

    email reset

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,85 +1,102 @@
-import React, { useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import app from "./Firebase.config";
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile } from 'firebase/auth'
+import app from './Firebase.config';
+import { Link } from 'react-router-dom';
+
+const auth = getAuth(app);
 
 const Register = () => {
-  const auth = getAuth(app);
-  //   const [email, setEmail] = useState("");
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-    console.log(email, password);
-    // validate
-    if (!/(?=.*[A-Z])/.test(password)) {
-      setError("Please add at least one uppercase");
-      return;
+    const handleSubmit = (event) => {
+        // 1. prevent page refresh
+        event.preventDefault();
+        setSuccess('');
+        setError('');
+        // 2. collect form data
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const name = event.target.name.value;
+        console.log(name, email, password)
+
+        // validate
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setError('Please add at least one uppercase');
+            return;
+        }
+        else if (!/(?=.*[0-9].*[0-9])/.test(password)) {
+            setError('Please add at least two numbers');
+            return
+        }
+        else if (password.length < 6) {
+            setError('Please add at least 6 characters in your password')
+            return;
+        }
+
+        // 3. create user in fb
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                setError('');
+                event.target.reset();
+                setSuccess('User has been created successfully');
+                sendVerificationEmail(result.user);
+                updateUserData(result.user, name);
+            })
+            .catch(error => {
+                console.error(error.message);
+                setError(error.message);
+            })
     }
-     
-    else if (!/(?=.*[0-9].*[0-9])/.test(password)) {
-      setError("Please add at least two numbers");
-      return;
-    } 
-    
-    else if (password.length < 6) {
-      setError("Please add at least 6 characters in your password");
-      return;
+
+    const sendVerificationEmail = (user) => {
+        sendEmailVerification(user)
+            .then(result => {
+                console.log(result);
+                alert('Please verify your email address')
+            })
     }
-    // create user in firebase
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((result) => {
-        const loggedUser = result.user;
-        console.log(loggedUser);
-        setError("");
-        event.target.reset();
-        // setSuccess('User has been successfully created')
-      })
-      .catch((error) => {
-        console.error(error.message);
-        setError(error.message);
-        setSuccess("");
-      });
-  };
 
-  const handleEmailChange = (event) => {
-    // console.log(event.target.value);
-    // setEmail(event.target.value);
-  };
+    const updateUserData = (user, name) => {
+        updateProfile(user, {
+            displayName: name
+        })
+            .then(() => {
+                console.log('user name updated')
+            })
+            .catch(error => {
+                setError(error.message);
+            })
+    }
 
-  const handlePasswordBlur = (event) => {
-    // console.log(event.target.value);
-  };
-  return (
-    <div>
-      <h2> Please Register </h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          onChange={handleEmailChange}
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Your Email"
-          required
-        />
-        <br />
-        <input
-          onBlur={handlePasswordBlur}
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Your Password"
-          required
-        />
-        <br />
-        <input type="submit" value="Register" />
-      </form>
-      <p className="text-danger">{error}</p>
-      <p className="text-success">{success}</p>
-    </div>
-  );
+    const handleEmailChange = (event) => {
+        // console.log(event.target.value);
+        // setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = (event) => {
+        // console.log(event.target.value);
+    }
+
+    return (
+        <div className='mx-auto'>
+            <h4>Please Register</h4>
+            <form onSubmit={handleSubmit}>
+                <input className='w-50 mb-4 rounded ps-2' type="text" name="name" id="name" placeholder='Your Name' required />
+                <br />
+                <input className='w-50 mb-4 rounded ps-2' onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email' required />
+                <br />
+                <input className='w-50 mb-4 rounded ps-2' onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' required />
+                <br />
+                <input className='btn btn-primary' type="submit" value="Register" />
+            </form>
+            <p><small>Already have an account? Please <Link to="/login">Login</Link> </small></p>
+            <p className='text-danger'>{error}</p>
+            <p className='text-success'>{success}</p>
+        </div>
+    );
 };
 
 export default Register;
